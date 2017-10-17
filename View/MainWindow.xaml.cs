85bf8830c7b979ec1c8eb4e5269b7d729eafc467
Thomas Roller <thomas.roller@advancis.de>
2017-10-17 08:52:49 +0200

    Refresh for LogWindow

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -115,7 +115,7 @@ namespace SolutionBuilder.View
         }
         private void MnuSettings_Click(object sender, RoutedEventArgs e)
         {
-            Window settings = new TreeSettings();
+            Window settings = new Settings();
             settings.ShowDialog();
         }
         private void MnuNewTab_Click(object sender, RoutedEventArgs e)
@@ -183,7 +183,18 @@ namespace SolutionBuilder.View
                 });
             }
         }
-
+        private void RefreshLog_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshLog();
+        }
+        public void RefreshLog()
+        {
+            BuildTabItem tab = (BuildTabItem)tabs.SelectedContent;
+            if ( tab.SelectedSolutionIndex != -1 )
+            {
+                _ViewModel.UpdateLog(tab.Solutions[tab.SelectedSolutionIndex]);
+            }
+        }
         private bool ExecuteDistribution(DistributionItem distribution, Executor executor, FileInfo copyExe)
         {
             if (!distribution.Checked || (!distribution.Copy && !distribution.Start))
